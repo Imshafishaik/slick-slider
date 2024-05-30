a91a73db99bbef82008c7f99260920631065f7dd
shaik shafi <shaik.shafi@careers360.in>
2024-05-30 17:29:23 +0530

    slick slider integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+import "./App.css";
+import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropleft } from "react-icons/io";
 
-function App() {
+
+export default function SimpleSlider() {
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="right_btn" onClick={onClick}><IoIosArrowDropright /> </div>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div  className="left_btn" onClick={onClick}><IoIosArrowDropleft /></div>
+    );
+  }
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slider {...settings}>
+      <div className="hello">
+        <h3>1</h3>
+      </div>
+      <div className="hello">
+        <h3>2</h3>
+      </div>
+      <div className="hello">
+        <h3>3</h3>
+      </div>
+      <div className="hello">
+        <h3>4</h3>
+      </div>
+      <div className="hello">
+        <h3>5</h3>
+      </div>
+      <div className="hello">
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
 }
-
-export default App;
